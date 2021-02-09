070116f32787f6b1c1304a9a0d4c90e06ed58886
Stacy Burris <stacy1burris@gmail.com>
2021-02-08 17:25:50 -0800

    get, post, put, delete routes work

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataEntry from "./components/DataEntry";
+
+function App() {
+  return (
+    <div className="App">
+      <DataEntry />
+    </div>
+  );
+}
+
+export default App;
