4b2b4c66383bb39498ffb5054c934edff0d1490d
Simon Panek <paneks19@gmail.com>
2021-02-10 23:34:04 -0800

    teacher lists dynamically rendering, coloring, and sorting

--- src/components/Teacher.js
+++ src/components/Teacher.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import Button from '@material-ui/core/Button';
 //import FormGroup from '@material-ui/core/FormGroup';
 //import Input from '@material-ui/core/Input';
 import { withRouter } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Chip from '@material-ui/core/Chip';
+import FaceIcon from '@material-ui/icons/Face';
 import superagent from 'superagent';
 import io from 'socket.io-client';
 import { connect } from 'react-redux';
@@ -12,6 +14,9 @@ const mapDispatchToProps = { populateStudents, updateStatus };
 
 const TeacherPage = (props) => {
   const [name, setName] = useState('');
+  const [pickupReadyStudents, setPickupReadyStudents] = useState([]);
+  const [releasedFromClassStudents, setReleasedFromClassStudents] = useState([]);
+  const [inClassStudents, setInClassStudents] = useState([]);
   // const [classRoster, setClassRoster] = useState([]);
   const teacherNameRef = React.createRef();
   const host = io.connect('http://localhost:3001', { transports: ['websocket'] });
@@ -24,6 +29,7 @@ const TeacherPage = (props) => {
   const updateStudent = () => {
     props.history.push('/dataEntry');
   }
+
   const classList = async (e) => {
     e.preventDefault();
     const teacherName = teacherNameRef.current.value;
@@ -71,6 +77,32 @@ const TeacherPage = (props) => {
 
   }
 
+  useEffect (()=> {
+    //generate list of students that have status of pickupReady
+    console.log('PRINCIPALPICKUP useEffect: before ', {pickupReadyStudents});
+    let studentsInProcess = props.classRoster.filter(student => {
+      if(student.studentStatus === 'pickupReady') return student;
+    }) 
+    setPickupReadyStudents(studentsInProcess);
+    console.log('PRINCIPALPICKUP useEffect: after ', {pickupReadyStudents});
+
+    //generate list of students that have a status of releasedFromClass
+    console.log('PRINCIPALPICKUP useEffect: before ', {releasedFromClassStudents});
+    let releasedStudents = props.classRoster.filter(student => {
+      if(student.studentStatus === 'releasedFromClass') return student;
+    }) 
+    setReleasedFromClassStudents(releasedStudents);
+    console.log('PRINCIPALPICKUP useEffect: after ', {releasedFromClassStudents});
+
+    //generate list of students that have a status of classRoom
+    console.log('PRINCIPALPICKUP useEffect: before ', {inClassStudents});
+    let waitingStudents = props.classRoster.filter(student => {
+      if(student.studentStatus === 'classRoom') return student;
+    }) 
+    setInClassStudents(waitingStudents);
+    console.log('PRINCIPALPICKUP useEffect: after ', {inClassStudents});
+  },[props.classRoster])
+
   useEffect(() => {
     console.log('TEACHER useEffect: ', 'props.classRoster', props.classRoster);
   })
@@ -82,16 +114,62 @@ const TeacherPage = (props) => {
       <Button onClick={updateStudent}>Update Student</Button>
 
       <form onSubmit={classList}>
-        <input onChange={searchName} type='text' ref={teacherNameRef} />
+        <input onChange={searchName} type='text' ref={teacherNameRef} placeholder="Teacher Last Name"/>
         <Button type='submit'>Submit</Button>
       </form>
-      <div>
+      {/* <div>
         {props.classRoster.map((student, idx) => (
           <div key={idx}>
             <p onClick={() => studentReleased(student)}>{student.name}</p>
           </div>
         ))}
-
+      </div> */}
+      <div>
+        <div>
+          {pickupReadyStudents.map((student, idx) => (
+            <div key={idx}>
+              <Chip
+                onClick={() => studentReleased(student)}
+                variant="outlined"
+                size="medium"
+                icon={<FaceIcon />}
+                label={`${student.name}`}
+                clickable
+                color="secondary"
+              />
+            </div>
+          ))}
+        </div>
+        <div>
+          {inClassStudents.map((student, idx) => (
+            <div key={idx}>
+              <Chip
+                onClick={() => studentReleased(student)}
+                variant="outlined"
+                size="medium"
+                icon={<FaceIcon />}
+                label={`${student.name}`}
+                clickable
+                color="gray"
+              />
+            </div>
+          ))}
+        </div>
+        <div>
+          {releasedFromClassStudents.map((student, idx) => (
+            <div key={idx}>
+              <Chip
+                onClick={() => studentReleased(student)}
+                variant="outlined"
+                size="medium"
+                icon={<FaceIcon />}
+                label={`${student.name}`}
+                clickable
+                color="primary"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
